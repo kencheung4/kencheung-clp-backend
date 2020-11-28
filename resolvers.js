00033ca69eb44b700880b828a345ed047506f447
Ken Cheung <z6ccn003@gmail.com>
2020-11-28 22:41:14 +0800

    finish the simple flow of graphql backend using serverless & aws app sync

---
+++ resolvers.js
@@ -0,0 +1,52 @@
+const { v4: uuidv4 } = require('uuid');
+
+const InMemoryStore = {
+  // [id]: [clicks]
+}
+
+const calculateGame = (id) => {
+  const red = InMemoryStore[id].clicks.filter(click => click.color === 'red').length;;
+  const blue = InMemoryStore[id].clicks.filter(click => click.color === 'blue').length;
+  InMemoryStore[id].red = red;
+  InMemoryStore[id].blue = blue;
+  InMemoryStore[id].black = blue - red;
+  return InMemoryStore[id];
+}
+
+const Query = {
+  getGame: async () => {
+    if (Object.keys(InMemoryStore).length !== 0) {
+      const id = Object.keys(InMemoryStore)[0];
+      return InMemoryStore[id];
+    }
+    const id = uuidv4();
+    InMemoryStore[id] = {
+      id: id,
+      red: 0,
+      blue: 0,
+      black: 0,
+      clicks: [],
+      createdAt: (new Date()).toISOString(),
+    }
+    return InMemoryStore[id];
+  }
+};
+
+const Mutation = {
+  clickBlue: async ({ id }) => {
+    InMemoryStore[id].clicks.push({
+      color: 'blue',
+      createdAt: (new Date()).toISOString(),
+    })
+    return calculateGame(id);
+  },
+  clickRed: async ({ id }) => {
+    InMemoryStore[id].clicks.push({
+      color: 'red',
+      createdAt: (new Date()).toISOString(),
+    })
+    return calculateGame(id);
+  },
+};
+
+module.exports = { Query, Mutation };
